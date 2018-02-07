796ba7c7e2f7d32f3b0e4eb4f44397b491681664
eliza29 <elizaseguraf@gmail.com>
2018-02-07 16:11:57 -0500

    autocomplatado listo

---
+++ js/app.js
@@ -0,0 +1,5 @@
+/* Funcionalidad del proyecto*/
+var inputFrom = document.getElementById('inputFrom');
+var inputTo = document.getElementById('inputTo');
+new google.maps.places.Autocomplete(inputFrom);
+new google.maps.places.Autocomplete(inputTo);
